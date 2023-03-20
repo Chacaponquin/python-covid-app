c088db5ee9ef4008183f9b01bd69c189aaa9a128
amayadf <amayadfuentes@gmail.com>
2023-03-20 17:27:35 -0400

    arreglos

--- ex2/functions/rowSort.py
+++ ex2/functions/rowSort.py
@@ -1,14 +1,15 @@
+from rich import print as rprint
 def rowSort(matrix):
     if len(matrix) == 0:
-        print('La matriz esta vacia. Llenar antes de llevar a cabo esta operacion')
+        rprint('La matriz esta vacia. Llenar antes de llevar a cabo esta operacion')
     else:
         rowIndex = int(input('Inserte el indice de la fila a ordenar: '))
 
-        while (not rowIndex in range(0, len(matrix))):
-            print('INDICE INVALIDO! No puede ser menor que cero o mayor que ' + str(len(matrix) - 1))
+        while rowIndex < 0 or rowIndex > len(matrix)-1:
+            rprint('El indice no puede ser menor que cero o mayor que ' + str(len(matrix) - 1))
             rowIndex = int(input('Inserte el indice de la fila a ordenar: '))
 
-        print('La fila ordenada de mayor a menor es: ' + str(sortRowGreaterToLower(matrix, rowIndex)))
+        rprint('La fila ordenada de mayor a menor es: ' + str(sortRowGreaterToLower(matrix, rowIndex)))
 
 
 def sortRowGreaterToLower(matrix, index):
